2a58cf781d8f614b66acb04edf70868b6a5bfd51
AtsuSuzu-R <suzuna.atsumi@jp.ricoh.com>
2025-10-08 09:56:52 +0900

    10/8 ShoppingList追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ShoppingList from "./ShoppingList";
+
+const data = [
+  { id: 1, item: "卵", quantity: 12, completed: false },
+  { id: 2, item: "牛乳", quantity: 1, completed: true },
+  { id: 3, item: "さんま", quantity: 4, completed: false },
+  { id: 4, item: "納豆", quantity: 2, completed: true },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ShoppingList items={data} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
